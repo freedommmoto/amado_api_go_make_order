82e1ef36330196f5e21354c5346db8fb4ae4aee4
Patara.m <patara.m@cartrack.com>
2022-07-05 15:46:56 +0700

    make main test for connect database and make first unit test product

---
+++ db/sqlc/product_test.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakeProduct(t *testing.T) {
+	RandomMakeProduct(t)
+}
+
+func RandomMakeProduct(t *testing.T) Product {
+	name := sql.NullString{
+		String: "asd",
+		Valid:  true,
+	}
+
+	arg := MakeNewProductParams{
+		Name:  name,
+		Stock: 1,
+		Price: 22,
+	}
+
+	product, err := testQueries.MakeNewProduct(context.Background(), arg)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, product)
+
+	assert.Equal(t, arg.Name, product.Name)
+	assert.Equal(t, arg.Stock, product.Stock)
+	assert.Equal(t, arg.Price, product.Price)
+
+	assert.NotZero(t, product.IDProduct)
+	assert.NotZero(t, product.CreatedAt)
+
+	return product
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,9 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
